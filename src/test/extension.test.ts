87dcd7907b99ed23b7093bdb0fe99eb1da588b55
Ludovic Hansen <ludovic.hansen@hotmail.be>
2026-01-30 20:43:59 +0100

    Remove unused imports

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,10 +2,6 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import { Parse } from './../parse';
-import { test } from 'mocha';
-
 
 suite('Extension Test Suite', () =>
 {
